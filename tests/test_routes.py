c40b81d7f55be2403b1cac2f1ef7b1b6e1232625
h7n0613 <xxu761634@gmail.com>
2024-12-30 22:23:39 -0500

    added build task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -57,6 +57,7 @@ class TestAccountService(TestCase):
 ######################################################################
 #  H E L P E R   M E T H O D S
 ######################################################################
+
     def _create_accounts(self, count):
         """Factory method to create accounts in bulk"""
         accounts = []
@@ -74,9 +75,10 @@ class TestAccountService(TestCase):
         return accounts
 
 
-    ######################################################################
-    #  A C C O U N T   T E S T   C A S E S
-    ######################################################################
+######################################################################
+#  A C C O U N T   T E S T   C A S E S
+######################################################################
+
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
